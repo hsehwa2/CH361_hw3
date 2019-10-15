b5db1b174107fa48e7d37740619155e94b82b2c7
hsehwa2 <hasan@DESKTOP-N1NP1U4.localdomain>
2019-10-14 21:44:13 -0500

    hello

--- hw3.c
+++ hw3.c
@@ -10,38 +10,6 @@
 #define MAXLINE 100
 
 
-// /* parseline - Parse the command line and build the argv array */
-// int parseline(char *buf, char **argv){
-//     char *delim;        /* Points to first space delimiter */
-//     int argc;           /* Number of args */
-//     int bg;             /* Background job? */
-//     buf[strlen(buf)-1] = ' ';   /* Replace trailing '\n' with space */
-//     while (*buf && (*buf == ' ')) /* ignores leading space */
-//         buf++;
-    
-//     /* Build the argv list */
-//     argc = 0;
-//     while ((delim = strchr(buf,' '))) {
-//         argv[argc++] = buf;
-//         char end[2] = "\0";
-        
-//         buf = end+ 1;
-//         while (*buf && (*buf == ' ')) /* Ignore spaces */
-//             buf++;
-//     }
-
-//     argv [argc] = NULL;
-
-//     if (argc == 0) /* Ignore blank line */
-//         return 1;
-    
-//     /* Should the job run in the background? */
-//     if ((bg = (*argv[argc-1] == '&')) != 0)
-//         argv[--argc] = NULL;
-    
-//     return bg;
-// }
-
 /* eval - Evaluate a command line */
 void eval(char *cmdline) {
     
@@ -63,8 +31,7 @@ void eval(char *cmdline) {
     childPid = fork();
 
     if (childPid == 0) {
-        execvp(args[0], args);
-        printf("error");      
+        execvp(args[0], args);    
     } else {
         wait(&status);
     }    
