2db3ece846cad06d35404e68137834b2c451ef33
hsehwa2 <hasan@DESKTOP-N1NP1U4.localdomain>
2019-10-14 22:55:56 -0500

    hello

--- hw3.c
+++ hw3.c
@@ -47,8 +47,8 @@ void eval(char *cmdline) {
     } else {
         wait(&status);
     }    
-    //pid_t Pid = getppid();
-    //printf("pid: %d status: %d", Pid, status);
+    pid_t Pid = getppid();
+    printf("pid: %d status: %d", Pid, status);
     return;
 }
 
